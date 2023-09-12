efd941370adb715af83ea3e16e0012ecd2ced312
Pete Laptop <pmugasa78@gmail.com>
2023-09-12 15:53:09 +0200

    added the basic form modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,134 +1,99 @@
+import { Plus } from "lucide-react";
 import { useState } from "react";
+import { Button, Modal } from "flowbite-react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-const App = () => {
-  const [date, setDate] = useState("");
-  const [client, setClient] = useState("");
-  const [engagement, setEngagement] = useState("");
-  const [hoursWorked, setHoursWorked] = useState(0);
-  const [tasks, setTasks] = useState([]);
+function App() {
+  const [selected, setSelected] = useState("");
+  const [fromTime, setFromTime] = useState("");
+  const [toTime, setToTime] = useState("");
+  const [openModal, setOpenModal] = useState();
+  const props = { openModal, setOpenModal };
 
-  class Task {
-    constructor(client, engagement, hoursWorked, date, id) {
-      this.client = client;
-      this.engagement = engagement;
-      this.hoursWorked = hoursWorked;
-      this.date = date;
-      this.id = id;
-    }
-  }
-
-  const handleForm = (e) => {
-    e.preventDefault();
-    const id = tasks.length;
-    const task = new Task(client, engagement, hoursWorked, date, id);
-    setTasks([...tasks, task]);
+  //form submission
+  const handleSubmit = (e) => {
+    alert("form submitted");
   };
 
-  //sort results by date
-  const sortedTasks = [...tasks].sort(
-    (objA, objB) => Number(objA.date) - Number(objB.date)
-  );
-
-  //store grouped clients
-  //TODO to be rendered as summary
-
-  const groupedClients = [];
-
-  for (const task of tasks) {
-    const client = task.client;
-
-    const existingClient = groupedClients.find(
-      (group) => group.client === client
-    );
-
-    //if client exist
-    if (existingClient) {
-      existingClient.totalHoursWorked =
-        Number(existingClient.totalHoursWorked) + Number(task.hoursWorked);
-    } else {
-      //client does not exist
-      [
-        ...groupedClients,
-        {
-          client: client,
-          engagement: task.engagement,
-          totalHoursWorked: task.hoursWorked,
-        },
-      ];
-    }
-  }
-
-  //check if date is in the current month
-  const isDateInCurrentMonth = (date) => {
-    const currentDate = new Date();
-
-    return (
-      date.getMonth() === currentDate.getMonth() &&
-      date.getFullYear() === currentDate.getFullYear()
-    );
-  };
-
-  console.log("grouped totals:", groupedClients);
   return (
     <>
-      <div className="w-full flex justify-center p-8">
-        <div className="w-80 h-80 border border-black p-2">
-          <form onSubmit={handleForm}>
-            <div className="flex flex-col space-y-4">
-              <input
-                type="text"
-                placeholder="client"
-                value={client}
-                onChange={(e) => setClient(e.target.value)}
-                className="border border-red-500 h-10"
-              />
-              <input
-                type="text"
-                placeholder="task"
-                value={engagement}
-                onChange={(e) => setEngagement(e.target.value)}
-                className="border border-red-500 h-10"
-              />
-              <DatePicker
-                selected={date}
-                onChange={(date) => setDate(date)}
-                className="border border-red-500 h-10"
-                name="date"
-                placeholderText="date"
-                filterDate={isDateInCurrentMonth}
-              />
-              <input
-                type="number"
-                placeholder="hours"
-                value={hoursWorked}
-                onChange={(e) => setHoursWorked(e.target.value)}
-                className="border border-red-500 w-20 h-10 "
-              />
+      <nav className="p-8 border-b border-gray-300 "></nav>
+      <main className=" p-4 mx-auto">
+        <Button onClick={() => props.setOpenModal("default")}>
+          Add task
+          <Plus size={16} className="my-auto ml-2 " />
+        </Button>
+        <Modal
+          show={props.openModal === "default"}
+          onClose={() => props.setOpenModal(undefined)}
+        >
+          <form onSubmit={handleSubmit}>
+            <Modal.Header>Task details</Modal.Header>
+            <Modal.Body>
+              <fieldset>
+                <input
+                  type="text"
+                  className="border border-gray-300 rounded-sm my-2 h-8 p-4 w-full placeholder:text-sm"
+                  placeholder="Enter task name"
+                />
+                <input
+                  type="text"
+                  className="border border-gray-300 rounded-sm my-2 h-8 p-4 w-full placeholder:text-sm"
+                  placeholder="Enter client name"
+                />
 
-              <button
+                <DatePicker
+                  className="border border-gray-300 rounded-sm my-2 h-8 p-4 w-full placeholder:text-sm"
+                  placeholderText="Date"
+                  selected={selected}
+                  onChange={(date) => setSelected(date)}
+                />
+                <div className="flex space-x-2 my-2">
+                  <DatePicker
+                    className="border border-gray-300 rounded-sm  h-8 p-4 w-full placeholder:text-sm"
+                    placeholderText="Start"
+                    selected={fromTime}
+                    onChange={(t) => setFromTime(t)}
+                    showTimeSelect
+                    showTimeSelectOnly
+                    timeIntervals={15}
+                    timeCaption="Time"
+                    dateFormat="h:mm aa"
+                  />
+                  <DatePicker
+                    className="border border-gray-300 rounded-sm  h-8 p-4 w-full placeholder:text-sm"
+                    placeholderText="Finish"
+                    selected={toTime}
+                    onChange={(t) => setToTime(t)}
+                    showTimeSelect
+                    showTimeSelectOnly
+                    timeIntervals={15}
+                    timeCaption="Time"
+                    dateFormat="h:mm aa"
+                  />
+                </div>
+              </fieldset>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button
                 type="submit"
-                className="px-1.5 py-0.5 bg-green-400 text-white"
+                onClick={() => props.setOpenModal(undefined)}
               >
-                add
-              </button>
-            </div>
-          </form>
+                Submit
+              </Button>
 
-          {sortedTasks.length > 0 &&
-            sortedTasks.map((task) => (
-              <div className="mt-16 flex justify-between" key={task.id}>
-                <p>{task.client}</p>
-                <p>{task.engagement}</p>
-                <p>{task.hoursWorked}</p>
-                <p>{task.date.toLocaleDateString()}</p>
-              </div>
-            ))}
-        </div>
-      </div>
+              <Button
+                color="gray"
+                onClick={() => props.setOpenModal(undefined)}
+              >
+                Cancel
+              </Button>
+            </Modal.Footer>{" "}
+          </form>
+        </Modal>
+      </main>
     </>
   );
-};
-
+}
 export default App;
